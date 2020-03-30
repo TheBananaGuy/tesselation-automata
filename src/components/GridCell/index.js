a101905722c285a006234a59354a901dbf3c8659
Sam Mullen <our.denouement@gmail.com>
2020-03-30 11:45:32 +0300

    GridCell reindex

--- src/components/GridCell/index.js
+++ src/components/GridCell/index.js
@@ -4,12 +4,14 @@ import './GridCell.scss';
 
 /**
  * Single cell component for the Game of Life simulation
+ *
  * @param {string} key Unique identifier of the cell
  * @param {bool} alive Current state of the cell
  * @param {string} onClick What haappens with the cell on a click
  */
 const GridCell = props => {
   const { key, alive, onClick } = props;
+
   return (
     <div
       key={key}
